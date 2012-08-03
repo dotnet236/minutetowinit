2a1c550f41872f2729d6abb1ac119be572701082
Anthony Capone <acapone@constantcontact.com>
2012-08-03 18:11:39 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Minutetowinit::Application.config.secret_token = '1a267c97fe312a664aa6a5e6c1633820b04a516001a764261af93d57e7b2c9ec474bcaf3bb907be62783b9072e705438d38a476ee30aab74123259cf7948040b'
